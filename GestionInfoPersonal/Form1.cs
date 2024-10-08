0dec385fe1acc39a5eff46c0aac68bec816bc4ad
danielcuervox <danielcuervox@hotmail.com>
2024-10-08 09:12:43 +0200

    finalizacion etiqueta 1

--- GestionInfoPersonal/Form1.cs
+++ GestionInfoPersonal/Form1.cs
@@ -16,5 +16,43 @@ namespace GestionInfoPersonal
         {
             InitializeComponent();
         }
+
+        private void rbMasculino_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (rbMasculino.Checked)
+            {
+                String genero = "masculino";
+            }
+            else if (rbMasculino.Checked) {
+                String genero = "femenino";
+            }
+
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendarFeNaci_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            monthCalendarFeNaci.RemoveAllBoldedDates();
+            DateTime fNaci = e.Start;
+            monthCalendarFeNaci.AddBoldedDate(fNaci);
+            monthCalendarFeNaci.UpdateBoldedDates();
+
+           
+
+        }
+
+        private void numericUpDownEdad_ValueChanged(object sender, EventArgs e)
+        {
+            decimal edad = numericUpDownEdad.Value;
+        }
     }
 }
